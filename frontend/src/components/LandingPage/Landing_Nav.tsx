6a778a476e5526194c351ccb9efa5c6c00147c38
bibash21-creator <bibashworkmail21@gmail.com>
2026-01-22 22:44:36 +0545

    Added next-themes

---
+++ frontend/src/components/LandingPage/Landing_Nav.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+
+import {useState, } from "react"
+
+import {FiSun, FiMoon} from "react-icons/fi"
+
+
+export default function Navbar(){
+    return(
+        <>
+
+
+            <nav className="navbar">
+                <div className="logo">
+                    Hamro Club
+                </div>
+            </nav>
+        
+        
+        
+        </>
+    )
+}
